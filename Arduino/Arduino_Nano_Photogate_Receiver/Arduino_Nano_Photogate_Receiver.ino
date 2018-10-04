f703a93d88b170db34c7a06f91a436f1253cbe3b
atouz18 <Atouz18@vt.edu>
2018-10-04 11:45:58 -0400

    These will be good. I promise. -ATT

---
+++ Arduino/Arduino_Nano_Photogate_Receiver/Arduino_Nano_Photogate_Receiver.ino
@@ -0,0 +1,111 @@
+/*
+  Andrew Touzinsky -- 10/04/18
+
+  This code is the receiver of the photogates. To specify what this code does, we setup the radio
+  communication with an address to the other tranceiver of 00101, and reads from the distance sensor
+  with a set threshhold value to determine when the photogate has been passed infront of. Then it
+  waits for the Arduino_Nano_Photogate_Sender to report the threshhold has been passed, and displays
+  the change in time on the LCD display.
+*/
+
+// include the library code:
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+//#include <time.h>
+
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00101";
+
+#define IRSensor A6 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
+int threshbump = 0;
+bool trigger = false;
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+int ReceivedMessage[1] = {000};
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+long starttime = 0;
+long elapsedtime = 0;
+bool endcrossed = true;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Time to 100ft:");
+
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1, address);
+  //radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+}
+
+void loop() {
+  // 5v
+  float volts = analogRead(IRSensor) * 0.0048828125; // value from sensor * (5/1024)
+  int distance = 13 * pow(volts, -1.1); // worked out from datasheet graph
+  int mean = 0; // every iteration resetting the mean value
+  for (int i = 1; i < 50; i++) { // this averages every 50 readings into one mean
+    mean = mean + distance;
+  }
+  mean = mean / 50; // this is the 2nd half of the averaging
+  Serial.println(mean);
+
+  // This section sets a threshhold value and looks for three sequential values that are
+  // below the threshhold, and if the third passes then the car is said to be infront.
+  if (mean < 20) { // 20 was chosen through experimentation, this is the minimum value
+    threshbump++;
+    if (threshbump >= 3) {
+      if (endcrossed == true) {
+        starttime = millis();
+        endcrossed == false;
+
+        for (int j = 0; j < 16; j++) {
+          lcd.setCursor(j, 1);
+          lcd.print("*");
+          delay(50);
+        }
+      }
+      //delay(750); // this is "debouncing"
+    }
+  }
+  else {
+    threshbump = 0;
+  }
+
+
+
+  //   set the cursor to column 0, line 1
+  //   (note: line 1 is the second row, since counting begins with 0):
+  if (radio.available()) {
+    if (endcrossed == false) {
+      elapsedtime = millis() - starttime;
+      lcd.setCursor(0, 1);
+      lcd.print("yeet");
+      lcd.print(elapsedtime);
+      endcrossed = true;
+    }
+
+
+  }
+  //  if (radio.available()) {
+  //    char text[32] = "";
+  //    radio.read(&text, sizeof(text));
+  //    Serial.println(text);
+  //    lcd.setCursor(0, 1);
+  //    //   print the number of seconds since reset:
+  //    lcd.print(text);
+  //
+  //  }
+  //  if (radio.available()) {
+  //    radio.read(ReceivedMessage, 1);
+  //    //if (ReceivedMessage[0] == 111){
+  //    lcd.setCursor(0, 1);
+  //    lcd.print("got something!");
+  //    //}
+}
---
+++ Arduino/Arduino_Nano_Photogate_Sender/Arduino_Nano_Photogate_Sender.ino
@@ -0,0 +1,65 @@
+/*
+  Andrew Touzinsky -- 10/04/18
+
+  This code is the sender of the photogates. To specify what this code does, we setup the radio
+  communication with an address to the other tranceiver of 00101, and reads from the distance sensor
+  with a set threshhold value to determine when the photogate has been passed infront of.
+*/
+
+// include the library code:
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define IRSensor A6 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "00101";
+int threshbump = 0;
+bool trigger = false;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+}
+
+void loop() {
+  // 5v
+  float volts = analogRead(IRSensor) * 0.0048828125; // value from sensor * (5/1024)
+  int distance = 13 * pow(volts, -1.1); // worked out from datasheet graph
+  int mean = 0; // every iteration resetting the mean value
+  //    Serial.println(distance);   // print the raw distance
+  for (int i = 1; i < 50; i++) { // this averages every 50 readings into one mean
+    mean = mean + distance;
+  }
+
+  mean = mean / 50; // this is the 2nd half of the averaging
+  //  if (mean > 100) { // this just makes the output look less funky, with no crazy high values
+  //    mean = 100;
+  //  }
+  //  if (mean < 0) { // this also makes the output correct, because why would it be negative...
+  //    mean = 0;
+  //  }
+
+  // This section sets a threshhold value and looks for three sequential values that are
+  // below the threshhold, and if the third passes then the car is said to be infront.
+  if (mean < 28) { // 28 was chosen through experimentation, this is the minimum value
+    threshbump++;
+    if (threshbump >= 3) {
+      trigger = true;
+      Serial.println("Car!");
+      const char text[] = "Car!";
+      radio.write(&text, sizeof(text));
+      delay(750); // this is "debouncing"
+    }
+    else {
+      trigger = false;
+    }
+  }
+  else {
+    threshbump = 0;
+  }
+  //  Serial.println(mean); //This is to view the values used for the threshold
+}
