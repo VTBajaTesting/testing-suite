cdf67c02ee2e6bdced9f0c65b4de70ea704160f6
hmatt12 <hmatt12@vt.edu>
2018-10-05 19:11:17 +0000

    added comments to the accelerometer code

--- Accelerometer/accel.cpp
+++ Accelerometer/accel.cpp
@@ -1,4 +1,6 @@
-
+//Accelorometer object code 
+//Written by: Matthew Herrity
+//Last Editted: 10/5/2018
 #include "accel.h"
 
 
@@ -10,9 +12,12 @@ Outputs
 */
 Accel::Accel(const char* devAdr, int r = 8)
 {
+//sets the device adress string to ' 0x1c' or ' 0x1d'
 strcpy(deviceADDR, " 0x");
 strcat(deviceADDR, devAdr);
+//sets the range equal to the parameter
 range = r;
+//uses if statements to setup the register equivalent to the inputed range
 if(range == 4)
 {
 r = 1;
@@ -27,8 +32,9 @@ range = 8;
 r = 2;
 }
 
+//writes the scaleing register to have the proper inputed scale
 set(deviceADDR, Mode_Control, r);
-
+//writes the sleep register to be in awake mode
 set(deviceADDR, PWR_Control, 1);
 
 }
@@ -41,19 +47,24 @@ string - the unproccessed string returned by the command 'should be in 0x-- form
 */
 
 string Accel::exec(char* cmd){
+//some processing variables
 string data = "";
 FILE * stream;
 const int max_buffer = 256;
 char buffer[max_buffer];
+// adds the final piece of the command for the read/write commands
 strcat(cmd, " 2>&1");
+//opens the results of the command after the command is run in linux 
 stream = popen(cmd, "r");
-
+//if the stream properly opened and the end of the file hasn't been reached add the next character to a beffer string
 if(stream){
 while(!feof(stream))
 if (fgets(buffer, max_buffer, stream) != NULL) data.append(buffer);
 
+//after the file is read close the file
 pclose(stream);
 }
+// return the read string *note -UNPROCESSED still must be converted
 return data;
 }
 
@@ -70,14 +81,19 @@ string - string of the command to be executed
 
 
 string Accel::get(char* reg1, const char* reg2){
+//processing variables: str - command to be processed	    str2 - result of command
 char str[100];
 string str2 = "";
 
+//chain together the appropriate registers and commands to get the desired string command
 strcpy(str, cmdGet);
 strcat(str, reg1);
 strcat(str, reg2);
+
+// executes command and gets the result
 str2 = exec(str);
 
+//return result
 return str2;
 }
 
@@ -94,14 +110,18 @@ string - string of the command to be executed
 */
 
 void Accel::set(char* reg1, const char* reg2, int value){
+// processing variables str - string of command to run			 str2 - result of command
 char str[100];
 string str2;
 
+//chains together command and resister adresses to get the command string
 strcpy(str, cmdSet);
 strcat(str, reg1);
 strcat(str, reg2);
 strcat(str, to_string(value).c_str());
 
+
+//execute command
 exec(str);
 
 }
@@ -116,7 +136,8 @@ exec(str);
 
 void Accel::read(){
 
-accel_x = (get(deviceADDR, accel_xH)) + (get(deviceADDR, accel_xL));
+
+//for each command read the registers containing the readings and smash togeter the necessary information
 accel_x = (get(deviceADDR, accel_xH)).substr(2, 2) + (get(deviceADDR, accel_xL)).substr(2, 1);
 accel_y = (get(deviceADDR, accel_yH)).substr(2, 2) + (get(deviceADDR, accel_yL)).substr(2, 1);
 accel_z = (get(deviceADDR, accel_zH)).substr(2, 2) + (get(deviceADDR, accel_zL)).substr(2, 1);
@@ -129,6 +150,7 @@ string - the signed hex number as a string
 
 string Accel::getX()
 {
+//return the x reading
 return accel_x;
 }
 
@@ -140,6 +162,7 @@ string - the signed hex number as a string
 
 string Accel::getY()
 {
+//return the y reading
 return accel_y;
 }
 
@@ -151,11 +174,13 @@ string - the signed hex number as a string
 
 string Accel::getZ()
 {
+//return the z reading
 return accel_z;
 }
 
 int Accel::getRange()
 {
+//return the range value
 return range;
 }
 
@@ -167,12 +192,16 @@ double - the reading of the acceleration in gs
 */
 double stringToGs (string str, Accel a)
 {
+	// convert the string of hex to an unsigned int
   int num = stoi(str, nullptr, 16);
+//convert the in to a double
   double reading = (double)(num);
+// if the most significant bit is one (>2048) the number is negative so we take the twos complement
  if ( reading >= 2048)
   {
     reading = reading - 4096;
   }
+// this divides by a conversion factor based on the range
   reading = reading /(2048  / a.getRange()) ;
   return reading;
 }
--- Accelerometer/accel.h
+++ Accelerometer/accel.h
@@ -11,18 +11,24 @@ using namespace std;
 
 #ifndef ACCEL
 #define ACCEL
-
+// this is the class definition
 class Accel
 {
+// these strings hold the readings
 string accel_x = "";
 string accel_y = "";
 string accel_z = "";
+// this holds the range
 int range = 8;
 
 //default 0x1D and 0x1
+//this holds the devices adress
 char* deviceADDR = new char[5];
+//address of the power control register
 const char* PWR_Control = " 0x2A ";
+//address of the xyz range control register
 const char* Mode_Control = " 0x0E ";
+//addresses of the acceleration readings
 const char* accel_xH = " 0x01";
 const char* accel_xL = " 0x02";
 
@@ -31,12 +37,13 @@ const char* accel_yL = " 0x04";
 const char* accel_zH = " 0x05";
 const char* accel_zL = " 0x06";
 
+//command strings the need registers to work
 const char* cmdGet = "i2cget -y 2";
 const char* cmdSet = "i2cset -y 2";
 
 public:
 
-
+//methods : see cpp file for more info
 Accel(const char*, int);
 string exec(char*);
 string get(char*, const char*);
@@ -50,4 +57,6 @@ int getRange();
 
 #endif
 
+
+//conversion function
 double stringToGs( string, Accel);
